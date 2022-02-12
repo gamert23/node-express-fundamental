98cbdf34eb42f983f53f2cf5303922b06184ab77
gamert23 <48782223+gamert23@users.noreply.github.com>
2022-02-12 18:51:50 +0700

    Create template html pages

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const chalk = require('chalk')
+const path = require('path')
+
 const app = express()
 const port = 3000
 
 // Manage request
+app.use(express.static(path.join(__dirname, '/public/')))
 app.get("/", (req, res) => {
   res.send('Hello test')
 })
